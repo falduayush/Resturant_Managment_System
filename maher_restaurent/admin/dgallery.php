4da51a28c4e23e4ab97c82721e8a2539ac78f8cc
Faldu_Ayush_1111 <113353948+falduayush@users.noreply.github.com>
2024-02-04 20:43:21 +0530

    Delete maher_restaurent directory

--- maher_restaurent/admin/dgallery.php
+++
@@ -1,8 +0,0 @@
-<?php
-$id = $_GET['id'];
-include "connect.php";
-mysqli_query($con,"delete from gallery where id='$id'");
-header("location:vgallery.php");
-
-
-?>
--- maher_restaurent/admin/footer.php
+++
@@ -1,2 +0,0 @@
-<br><br>
-<div class="footer"> Admin Side Login</div>
